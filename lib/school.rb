cd05206b33cf6a4a2f71a3a42f61c2cd3182d70f
aromsa <annacbateman@gmail.com>
2020-04-21 19:36:16 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,10 @@
-# code here!
+class School
+    attr_accessor :roster, :add_student
+    def initialize (roster = 0)
+        @roster = roster
+    end
+    def add_student(student, grade)
+        @student
+        @grade
+    end
+end
