0119edb8928cf25a5954ab2b9d932ea7019472dd
FCGit19 <canofred@gmail.com>
2020-04-21 23:52:58 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+
+class School
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def roster
+    school_roster = {}
+
+        
+  end
+
+  def add_student(student, grade)
+
+    if !school_roster[grade] 
+        school_roster[grade] = []
+    else 
+        school_roster[grade] << student
+    end 
+  end 
+    
+end 
+
+school = School.new("Bayside High School")
