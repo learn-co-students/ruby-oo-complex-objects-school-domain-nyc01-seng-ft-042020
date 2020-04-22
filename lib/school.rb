1c69f01cfa71ba93853066ea6a5ee88160390686
Sofia Tofigh <sofiatofigh0@gmail.com>
2020-04-21 23:12:51 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+
+    def initialize(name,roster={})
+        @name=name
+        @roster=roster
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student_name,grade)
+        @roster[grade]=[]
+        @roster[grade]<<student_name
+    end
+
+    def grade(grade)
+       @roster[grade]
+    end
+
+    def sort
+        @roster.each do |key, value|
+          value.sort!
+        end
+      end
+
+end
