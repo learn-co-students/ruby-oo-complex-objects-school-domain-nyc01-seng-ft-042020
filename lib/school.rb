016a1ea072d15513937929d0d4a42073f18ba059
ryanmeinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-21 22:12:26 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+class School
+  attr_accessor :roster, :student, :grade
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student(student, grade)
+    @student = student
+    @grade = grade
+    @roster[grade] ||= []
+    @roster[grade] << student
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+    # @roster.sort_by {|k, v| [k, v]}.to_h
+    # @roster.map {|k, v| [k, v]}.sort.to_h
+    # temp = @roster.sort_by {|k, v| k}.to_h
+    @roster.map {|k,v| [k, v.sort]}.to_h
+    # @roster.sort_by {|k,v| v.sort}.to_h
+  end
+end
