bbaac25b587385da0008f68d9497eddb08e99c39
FCGit19 <canofred@gmail.com>
2020-04-22 10:48:47 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,24 +1,62 @@
+require 'pry'
+
+# class School
+#   def initialize(name)
+#     @name = name
+#     @roster = {}
+#   end
+  
+#   def add_student(student, grade)
+
+#     if roster[grade] ||= student 
+#         roster[grade] << student 
+#     end 
+    
+#   end 
+
+# end 
+
 
 class School
   def initialize(name)
     @name = name 
+    @roster = {} #this allows each school that's initialized to have an empty roster. by putting here, this gives you want you need as soon as initialize is created
   end 
-  
-  def roster
-    school_roster = {}
 
-        
-  end
+def name 
+    @name
+end 
+
+def roster
+    @roster
+end 
+
+def roster=(roster)
+    @roster = roster 
+end 
+
+def grade(grade)
+  @roster[grade]
+end 
 
   def add_student(student, grade)
-
-    if !school_roster[grade] 
-        school_roster[grade] = []
+    
+    if !@roster[grade] 
+        @roster[grade] = []
+        @roster[grade] << student
     else 
-        school_roster[grade] << student
+        @roster[grade] << student
     end 
+  end 
+
+  def sort
+    @roster.map do |grade, student|
+      @roster[grade] = student.sort 
+    end 
+    @roster 
   end 
     
 end 
 
 school = School.new("Bayside High School")
+
