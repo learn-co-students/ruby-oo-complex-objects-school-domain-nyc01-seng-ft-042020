a7c97b4031382135a1b70d242da8f1b61522f0a2
blobbyblobfish <aywang6@gmail.com>
2020-04-21 21:35:30 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+        def initialize(school_name)
+            @school_name = school_name
+            @roster = {}
+        end
+
+        def roster 
+          @roster
+        end
+
+        def add_student (name, grade)
+            roster[grade] ||= []
+            roster[grade] << name
+            roster
+        end
+
+        def grade (grade)
+          roster[grade]
+        end
+
+        def sort
+          new_sorted_hash = {}
+            roster.each do |grade, name|
+                new_sorted_hash[grade] = name.sort
+              end
+          new_sorted_hash 
+        end
+
+end
